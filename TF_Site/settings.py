f516a035aa104311bff0901a05cc333c88502125
Tiago Farinha <tiagomnf1999@gmail.com>
2020-01-24 03:04:08 +0000

    Small fixes

--- TF_Site/settings.py
+++ TF_Site/settings.py
@@ -100,6 +100,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = "static_local"
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
